b5b2975da8f18ce21c34b471f8fac03cbd9c2e90
croky-b <64493706+croky-b@users.noreply.github.com>
2020-11-04 19:19:19 +0100

    Add files via upload

---
+++ sounds/TurboWhistleDummy.h
@@ -0,0 +1,3 @@
+const unsigned int turboSampleRate = 22050;
+const unsigned int turboSampleCount = 2;
+const signed char turboSamples[] = {0,0,};
---
+++ sounds/boat/AIR_STOP_DUMMY.h
@@ -0,0 +1,3 @@
+const unsigned int starterStopSampleRate = 22050;
+const unsigned int starterStopSampleCount = 2;
+const signed char starterStopSamples[] = {0,0,};
