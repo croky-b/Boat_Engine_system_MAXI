72f1aa3d4ba4d40e1ba8e5e826bf0c9bdd9478fc
croky-b <henri_oul@hotmail.com>
2022-03-25 15:25:00 +0100

    Add files via upload

--- 1-Setup.h
+++ 1-Setup.h
@@ -1,10 +1,10 @@
 #include "XT_DAC_Audio.h"
+
 //______________________________________________________________________________
 //-------------------------------VOLUME----------------------------------
 //_______________________________________________________________________________
 
-
-#define ANCOR_VOL 64 
+#define ANCOR_VOL 127 
 #define SHORT_VOL 127 
 #define LONG_VOL 127 
 #define GUN_VOL 127 
@@ -14,7 +14,7 @@
 #define ALARM2_VOL 127 
 #define BELL_VOL 127 
 #define SEAGULL_VOL 127 
-#define AMBIENT_VOL 64 
+#define AMBIENT_VOL 127 
 #define TWELVE1_VOL 127 
 #define TWELVE2_VOL 127 
 #define TWELVE3_VOL 127 
@@ -26,7 +26,6 @@
 #define ELEVEN6_VOL 127 
 #define ELEVEN7_VOL 127 
 #define ELEVEN8_VOL 127 
-
 //______________________________________________________________________________
 //-------------------------------COMMUNICATION----------------------------------
 //_______________________________________________________________________________
@@ -103,14 +102,14 @@ const boolean CH1_ENABLE = true;
 const boolean CH2_ENABLE = true;   
 const boolean CH3_ENABLE = true;   
 const boolean CH4_ENABLE = true;   
-const boolean CH5_ENABLE = true;   
-const boolean CH6_ENABLE = true;   
-const boolean CH7_ENABLE = true;   
-const boolean CH8_ENABLE = true;   
-const boolean CH9_ENABLE = true;   
-const boolean CH10_ENABLE = true;   
-const boolean CH11_ENABLE = true;   
-const boolean CH12_ENABLE = true;   
+const boolean CH5_ENABLE = false;   
+const boolean CH6_ENABLE = false;   
+const boolean CH7_ENABLE = false;   
+const boolean CH8_ENABLE = false;   
+const boolean CH9_ENABLE = false;   
+const boolean CH10_ENABLE = false;   
+const boolean CH11_ENABLE = false;   
+const boolean CH12_ENABLE = false;   
 const boolean CH13_ENABLE = false;   
 const boolean CH14_ENABLE = false;   
 const boolean CH15_ENABLE = false;   
@@ -142,23 +141,8 @@ XT_Wav_Class Start(Start_Data);
 //______________________________________________________________________________
 //---------------------------------ACTION1---------------------------------------
 //_______________________________________________________________________________
-#define ONTIME_1 30
-#define OFFTIME_1 500
-#define ACTION1A_FLASH_4
-#define ACTION1B_3
-#define ACTION1C_1
-#define ACTION1_ENABLE
-#define ACTION1CH 6
-#define ACTION1ON 3
-#define ACTION1OFF 2
-#define ACTION1NA 1
-#define ACTION1_MULTI
-#define ACTION1_SWITCH1 1
-#define ACTION1_SWITCH2 2
-#define ACTION1_SWITCH3 3
-#define ACTION1_SWITCH4 4
-#define ACTION1_SWITCH5 5
-#define ACTION1_END_SWITCH 6
+#define ONTIME_1 250
+#define OFFTIME_1 250
 
 
 //______________________________________________________________________________
@@ -168,98 +152,50 @@ XT_Wav_Class Start(Start_Data);
 XT_Wav_Class Ancre(Ancre_Data);
 #define ANCOR_SPEED_UP  1250
 #define ANCOR_SPEED_DOWN  1750
-#define AUTOMATIC_ANCHOR
-#define ANCHORMOTOR
-#define ANCHORMOTOR_6
-#define ANCHORSOUND
-#define INTER
-#define ACTION2_ENABLE
-#define ACTION2CH 6
-#define ACTION2NA 3
-#define ACTION2OFF 2
-#define ACTION2ON 1
 
 //______________________________________________________________________________
 //---------------------------------ACTION3---------------------------------------
 //_______________________________________________________________________________
 #include "sounds/boat/AuxSound/Bell.h"
 XT_Wav_Class Bell(Bel_Data);
-#include "sounds/boat/AuxSound/Short_blast9.h"
+#include "sounds/boat/AuxSound/Short_blast1.h"
 XT_Wav_Class Short_blast(Short_Data);
-#include "sounds/boat/AuxSound/Long_blast9.h"
+#include "sounds/boat/AuxSound/Long_blast1.h"
 XT_Wav_Class Long_blast(LongB_Data);
-#define FISHINGBOAT
-#define ACTION3_ENABLE
-#define ACTION3CH 7
-#define ACTION3ON 3
-#define ACTION3OFF 2
-#define ACTION3NA 1
 
 //______________________________________________________________________________
 //---------------------------------ACTION4---------------------------------------
 //_______________________________________________________________________________
-#define ACTION4_ENABLE
-#define ACTION4CH 5
-#define ACTION4NA 3
-#define ACTION4OFF 2
-#define ACTION4ON 1
 
 //______________________________________________________________________________
 //---------------------------------ACTION5---------------------------------------
 //_______________________________________________________________________________
-#define ACTION5_ENABLE
-#define ACTION5CH 5
-#define ACTION5ON 3
-#define ACTION5OFF 2
-#define ACTION5NA 1
 
 //______________________________________________________________________________
 //---------------------------------ACTION6---------------------------------------
 //_______________________________________________________________________________
-#define SEQ6_DURATION 10000
-#define SEQ6B_DURATION 10000
+#define SEQ6_DURATION 1000
+#define SEQ6B_DURATION 1000
 #include "sounds/boat/AuxSound/AAGun_Large.h"
 XT_Wav_Class AAGun(AAGun_Data);
-#define ACTION6_ENABLE
-#define ACTION6CH 8
-#define ACTION6ON 3
-#define ACTION6OFF 2
-#define ACTION6NA 1
-#define ACT6_SERVO_SEQ
-#define SEQ6_PART1_GUN
 
 //______________________________________________________________________________
 //---------------------------------ACTION7---------------------------------------
 //_______________________________________________________________________________
-#define SEQ7_DURATION 15000
-#define SEQ7B_DURATION 10000
+#define SEQ7_DURATION 1000
+#define SEQ7B_DURATION 1000
 #include "sounds/boat/AuxSound/Gun.h"
 XT_Wav_Class Gun(Gun_Data);
-#define SEQ7_PART1_AAGUN
-#define ACTION7_ENABLE
-#define ACTION7CH 8
-#define ACTION7NA 3 
-#define ACTION7OFF 2
-#define ACTION7ON 1
-#define ACT7_SERVO_SEQ
 
 //______________________________________________________________________________
 //---------------------------------ACTION8---------------------------------------
 //_______________________________________________________________________________
-#define RINGALARM
 #include "sounds/boat/AuxSound/Alarm.h"
 XT_Wav_Class Alarm(Alarm_Data);
 #include "sounds/boat/AuxSound/Ring.h"
 XT_Wav_Class Ring(Ring_Data);
-#define ACT8_SERVO_MANUAL
-#define ACTION8_ENABLE
-#define ACTION8CH 9
-#define ACTION8ON 3
-#define ACTION8NA 2
-#define ACTION8OFF 1
 #define ONTIME_8 250
 #define OFFTIME_8 250
-
 
 
 //______________________________________________________________________________
@@ -267,7 +203,6 @@ XT_Wav_Class Ring(Ring_Data);
 //_______________________________________________________________________________
 #define ONTIME_9 250
 #define OFFTIME_9 250
-
 volatile boolean AutoLight =true;
 
 //______________________________________________________________________________
@@ -298,21 +233,6 @@ XT_Wav_Class eleven8(eleven8_Data);
 XT_Wav_Class Seagull(Seagull_Data);
 #include "sounds/boat/AuxSound/Ambient.h"
 XT_Wav_Class Ambient(Ambient_Data);
-
-
-#define ACTION11_ENABLE
-#define ACTION11A_ENABLE
-#define ACTION11ACH 10
-#define ACTION11A_HIGH_SEAGULL
-#define ACTION11A_HIGH_REPEAT 0
-#define ACTION11A_LOW_PERSO_3
-#define ACTION11A_LOW_REPEAT 0
-#define ACTION11B_ENABLE
-#define ACTION11BCH 12
-#define ACTION11B_HIGH_AMBIENT
-#define ACTION11B_HIGH_REPEAT_FOREVER
-
-
 //______________________________________________________________________________
 //---------------------------------ACTION12---------------------------------------
 //_______________________________________________________________________________
@@ -324,34 +244,6 @@ XT_Wav_Class twelve2(twelve2_Data);
 XT_Wav_Class twelve3(twelve3_Data);
 #define SERVO_MIN 500
 #define SERVO_MAX 2400
-#define ACT12_ACT1_SWITCH
-#define ACT12SERVOSWITCH
-#define SERVO1_HOME
-#define SERVO2_HOME
-#define SERVO3_HOME
-#define SERVO4_HOME
-#define SERVO5_HOME
-#define SERVO6_HOME
-
-#define SERVO1_SWITCH 1
-#define SERVO2_SWITCH 2
-#define SERVO3_SWITCH 3
-#define SERVO4_SWITCH 4
-#define SERVO5_SWITCH 5
-#define SERVO6_SWITCH 6
-#define END_SWITCH 7
-#define ACTION_SERVO1 1
-#define ACTION_SERVO2 2
-#define ACTION_SERVO3 99
-#define ACTION_SERVO4 99
-#define ACTION_SERVO5 99
-#define ACTION_SERVO6 99
-#define ACTION12_ENABLE
-
-#define ACTION12CH 11
-#define ACTION12ON 3
-#define ACTION12NA 2
-#define ACTION12OFF 1
 int16_t SERVO_START[7] {1500,1450,1450,1450,1450,1450,1450}; 
 //______________________________________________________________________________
 //---------------------------------SEQUENCE---------------------------------------
@@ -397,5 +289,3 @@ int16_t SERVO_START[7] {1500,1450,1450,1450,1450,1450,1450};
 #define OFFTIME_SEQ2B_PART3 250
 #define ONTIME_SEQ2B_PART4 250
 #define OFFTIME_SEQ2B_PART4 250
-#define SEQ1_CH_0
-#define SEQ2_CH_0
